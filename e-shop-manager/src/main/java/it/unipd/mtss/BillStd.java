83d9448d3d37fa75dcd36cd291770e8dbfb6e2e2
uiob <uiob1210@gmail.com>
2022-05-19 22:15:53 +0200

    fixed issues

--- e-shop-manager/src/main/java/it/unipd/mtss/BillStd.java
+++ e-shop-manager/src/main/java/it/unipd/mtss/BillStd.java
@@ -16,7 +16,9 @@ public class BillStd implements Bill {
         int i = 0;
         double tot = 0;
         while(i < itemsOrdered.size()){
-            if(itemsOrdered.get(i).getPrice() < 0) throw new BillException("invalid price value: negative value");
+            if(itemsOrdered.get(i).getPrice() < 0) {
+                throw new BillException("invalid price value: negative value");
+            }
             else{
             tot += itemsOrdered.get(i).getPrice();
             i++;
